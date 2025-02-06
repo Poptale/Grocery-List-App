612f08033a4670a71f82f9bef7d66cae4a1dcec7
Poptale <pratik04jha@gmail.com>
2025-02-06 22:25:03 +0530

    changes for correction

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
 import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
-
 const appSettings = {
     databaseURL: "https://playground-511f2-default-rtdb.asia-southeast1.firebasedatabase.app/"
 };
-
 const app = initializeApp(appSettings);
 const database = getDatabase(app);
 const listDB = ref(database, "shoppingList");
-
 const input = document.querySelector("input");
 const button = document.querySelector("button");
 const list = document.querySelector(".list");
@@ -48,6 +45,5 @@ function appendLi(itemId, itemValue) {
         let exactLocation = ref(database, `shoppingList/${itemId}`);
         remove(exactLocation);
     });
-
     list.appendChild(newLi);
 };
